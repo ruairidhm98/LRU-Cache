f2d9f277b950d1077dc60a5b22714ed51a2385b5
ruairidhm98 <2250079m@student.gla.ac.uk>
2021-01-09 21:23:00 +0000

    Forgot to add template paramters to function definitions, whoops

--- src/lruCache.cpp
+++ src/lruCache.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 template <unsigned CacheSize, unsigned CacheLineSize, unsigned verbose>
-LRUCache::LRUCache()
+LRUCache<CacheSize,CacheLineSize,verbose>::LRUCache()
   : CL_MASK(CacheLineSize-1)
   , CL_SHIFT(log(CACHELINE_SZ)/log(2))
   , m_lastUsedLoc(CacheSize/CacheLineSize - 1)
@@ -16,14 +16,14 @@ LRUCache::LRUCache()
 
 // Initialises all the free locations in the stack to keep track of them
 template <unsigned CacheSize, unsigned CacheLineSize, unsigned verbose>
-inline void LRUCache::initLocationStack()
+inline void LRUCache<CacheSize,CacheLineSize,verbose>::initLocationStack()
 {
   iterCache([this](uint32_t const& i) { m_locationStack.push(i); });
 }
 
 // If we wish to print an error message determined by template parameter verbose
 template <unsigned CacheSize, unsigned CacheLineSize, unsigned verbose>
-inline void LRUCache::writeMessage(const char* message) const
+inline void LRUCache<CacheSize,CacheLineSize,verbose>::writeMessage(const char* message) const
 {
   if constexpr(verbose)
   {
@@ -34,7 +34,7 @@ inline void LRUCache::writeMessage(const char* message) const
 // Common loop pattern across several functions encapsulated in own function
 template <unsigned cachesize, unsigned cachelinesize, unsigned verbose>
 template <typename Func>
-inline void LRUCache::iterCache(Func&& f)
+inline void LRUCache<CacheSize,CacheLineSize,verbose>::iterCache(Func&& f)
 {
   for (auto i = 0UL; i < CacheSize; i+=CacheLineSize)
   {
@@ -44,7 +44,7 @@ inline void LRUCache::iterCache(Func&& f)
 
 // Flush the contents of the cache - evicts all data to RAM and resets state
 template <unsigned cachesize, unsigned cachelinesize, unsigned verbose>
-void LRUCache::flush(int ram[])
+void LRUCache<CacheSize,CacheLineSize,verbose>::flush(int ram[])
 {
   writeMessage("Flushing cache");
   iterCache([this,ram](uint32_t const& i) { writeToMemOnEvict(ram, i); });
